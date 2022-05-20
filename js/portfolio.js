02fe9c92f2a3c9cd11df58f11b5a495d1651ae86
Val-254 <irunguvalentine73@gmail.com>
2022-05-20 15:45:45 +0300

    Added JavaScript code

---
+++ js/portfolio.js
@@ -0,0 +1,22 @@
+function myFunction() {
+    input("Enter your email for my weekly newsletter");
+	alert("Hello!");
+	alert("How are you?");
+    alert("Welcome to Valentine Irungu's Personal Portfolio");
+    
+}    
+
+function changeText() {
+	var span = document.getElementById("output");
+	var textBox = document.getElementById("textbox");
+	
+	 textbox.style.color = "red"; 
+		
+}
+
+function changeImage() {
+	var ValsImg = document.getElementById("ValsImg");
+	
+	ValsImg.style.float = "left"; 
+		
+}
